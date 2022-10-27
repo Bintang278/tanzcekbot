48c45aef3a4eb6f888172ab35cbeac6f395649db
Bintang278 <116737200+Bintang278@users.noreply.github.com>
2022-10-27 10:08:15 +0700

    Update Default_Github.lua

--- Default_Github.lua
+++ Default_Github.lua
@@ -1,16 +1,24 @@
 Rdp = {
-    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
-    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
-    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
-    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
-    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
-    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
-    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
-    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
-    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
-    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
-    {nordp = 6, botname = "Bot6", Status = "Nonaktif"},
-    {nordp = 6, botname = "Bot6", Status = "Nonaktif"}
+    {nordp = 1, botname = "tanzbgl131", Status = "Nonaktif"},
+    {nordp = 1, botname = "tanzbgl109", Status = "Nonaktif"},
+    {nordp = 2, botname = "tanzbgl110", Status = "Nonaktif"},
+    {nordp = 2, botname = "tanzbgl132", Status = "Nonaktif"},
+    {nordp = 3, botname = "tanzbgl123", Status = "Nonaktif"},
+    {nordp = 3, botname = "tanzbgl111", Status = "Nonaktif"},
+    {nordp = 4, botname = "tanzbgl112", Status = "Nonaktif"},
+    {nordp = 4, botname = "tanzbgl124", Status = "Nonaktif"},
+    {nordp = 5, botname = "tanzbgl125", Status = "Nonaktif"},
+    {nordp = 5, botname = "tanzbgl103", Status = "Nonaktif"},
+    {nordp = 6, botname = "tanzbgl93", Status = "Nonaktif"},
+    {nordp = 6, botname = "tanzbgl126", Status = "Nonaktif"}
+    {nordp = 7, botname = "tanzbgl127", Status = "Nonaktif"},
+    {nordp = 7, botname = "tanzbgl120", Status = "Nonaktif"},
+    {nordp = 8, botname = "tanzbgl128", Status = "Nonaktif"},
+    {nordp = 8, botname = "tanzbgl116", Status = "Nonaktif"},
+    {nordp = 9, botname = "tanzbgl117", Status = "Nonaktif"},
+    {nordp = 9, botname = "tanzbgl129", Status = "Nonaktif"},
+    {nordp = 10, botname = "tanzbgl108", Status = "Nonaktif"},
+    {nordp = 10, botname = "tanzbgl130", Status = "Nonaktif"},
 }
 
 ShowIP = true
@@ -20,6 +28,11 @@ ip_add = {
     "Slot 5,6",
     "Slot 7,8",
     "Slot 9,10",
+    "Slot 11,12",
+    "Slot 13,14",
+    "Slot 15,16",
+    "Slot 17,18",
+    "Slot 19,20",
 }
 
 hookURL = "https://discord.com/api/webhooks/1027802366113755146/bHnZ7UQoLn3xSTFBweeMrEjw-tfdbjgPFR8pH64rGv2PRceQ3KrahsMMHWi7mHys_XZH"
@@ -30,4 +43,4 @@ Teks_Footer = "Golek Duit Kanggo Masa Depan"
 Emote_Offline = "<:offline:982957367392088124>"
 Emote_Online = "<:online:982957403756716052>"
 
-menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
+menit = 10 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
