831055037dd30fd7d99a91e32571a6392bc6893c
Bintang278 <116737200+Bintang278@users.noreply.github.com>
2022-10-28 12:35:52 +0700

    Update Default_Github.lua

--- Default_Github.lua
+++ Default_Github.lua
@@ -1,21 +1,21 @@
 Rdp = {
     {nordp = 1, botname = "tanzbgl131", Status = "Nonaktif"},
     {nordp = 1, botname = "tanzbgl109", Status = "Nonaktif"},
-    {nordp = 2, botname = "tanzbgl110", Status = "Nonaktif"},
+    {nordp = 2, botname = "tanzbgl133", Status = "Nonaktif"},
     {nordp = 2, botname = "tanzbgl132", Status = "Nonaktif"},
     {nordp = 3, botname = "tanzbgl123", Status = "Nonaktif"},
     {nordp = 3, botname = "tanzbgl111", Status = "Nonaktif"},
-    {nordp = 4, botname = "tanzbgl112", Status = "Nonaktif"},
-    {nordp = 4, botname = "tanzbgl124", Status = "Nonaktif"},
+    {nordp = 4, botname = "tanzbgl134", Status = "Nonaktif"},
+    {nordp = 4, botname = "tanzbgl135", Status = "Nonaktif"},
     {nordp = 5, botname = "tanzbgl125", Status = "Nonaktif"},
     {nordp = 5, botname = "tanzbgl103", Status = "Nonaktif"},
     {nordp = 6, botname = "tanzbgl93", Status = "Nonaktif"},
     {nordp = 6, botname = "tanzbgl126", Status = "Nonaktif"}
     {nordp = 7, botname = "tanzbgl127", Status = "Nonaktif"},
-    {nordp = 7, botname = "tanzbgl120", Status = "Nonaktif"},
-    {nordp = 8, botname = "tanzbgl128", Status = "Nonaktif"},
+    {nordp = 7, botname = "tanzbgl136", Status = "Nonaktif"},
+    {nordp = 8, botname = "tanzbgl137", Status = "Nonaktif"},
     {nordp = 8, botname = "tanzbgl116", Status = "Nonaktif"},
-    {nordp = 9, botname = "tanzbgl117", Status = "Nonaktif"},
+    {nordp = 9, botname = "tanzbgl138", Status = "Nonaktif"},
     {nordp = 9, botname = "tanzbgl129", Status = "Nonaktif"},
     {nordp = 10, botname = "tanzbgl108", Status = "Nonaktif"},
     {nordp = 10, botname = "tanzbgl130", Status = "Nonaktif"},
@@ -43,4 +43,4 @@ Teks_Footer = "Golek Duit Kanggo Masa Depan"
 Emote_Offline = "<:offline:982957367392088124>"
 Emote_Online = "<:online:982957403756716052>"
 
-menit = 10 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
+menit = 1 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
